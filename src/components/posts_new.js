3502bca0fc2af8b7492177a65b554fc4ff07cc0d
hoslack <hoslackochieng@gmail.com>
2018-01-04 15:06:45 +0200

    front end validation created

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 	renderField(field) {
+		const { meta: { touched, error } } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input type="text" {...field.input} className="form-control" />
-				{field.meta.error}
+				<div className="text-help">{touched ? error : ''}</div>
 			</div>
 		);
 	}
+	onSubmit(values) {
+		console.log(values);
+	}
 
 	render() {
+		const { handleSubmit } = this.props;
 		return (
 			<div>
-				<form>
+				<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 					<Field label="Title" name="title" component={this.renderField} />
 					<Field label="Categories" name="categories" component={this.renderField} />
 					<Field label="Content" name="content" component={this.renderField} />
 					<button type="submit" className="btn btn-primary">
 						Submit
 					</button>
+					<Link className="btn btn-danger" to="/">
+						Cancel
+					</Link>
 				</form>
 			</div>
 		);
@@ -40,6 +50,7 @@ const validate = values => {
 	if (!values.content) {
 		errors.content = 'Please Enter a Content';
 	}
+	return errors;
 };
 
 export default reduxForm({
